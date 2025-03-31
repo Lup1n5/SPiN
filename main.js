5d80a44a9f379b5288f3adf1986f23b58a8209b7
Lup1n5 <146459441+Lup1n5@users.noreply.github.com>
2025-03-31 23:44:13 +0000

    d

--- main.js
+++ main.js
@@ -48,9 +48,11 @@ function logout() {
     set(pingsRef, null)
     const usersRef = ref(db,`users/${user}`)
     set(usersRef, null)
-  }, 100).then(() => {location.reload(true);})
+  }, 100).then(() => {location.reload(true);
     loggedInView.style.display = 'none' 
     loggedOutView.style.display = 'block'
+  })
+    
   
 }
 function checkAdmPings() {
